bf0d95608bcf3990d268dc19254a6315463e618d
m3rzb0w <80rN70c0D3@protonmail.com>
2023-08-30 01:50:46 +0000

    maze

---
+++ app.py
@@ -0,0 +1,153 @@
+import pygame
+import sys
+#DFS
+#BFS
+#Dijkstra
+#AStar
+
+window_width = 800
+window_height = 800
+
+columns = 25
+rows = 25
+
+grid = []
+queue = []
+path = []
+
+square_width = window_width // columns
+sqauqre_height = window_height // rows
+
+class Square:
+    def __init__(self, i, j):
+        self.x = i
+        self.y = j
+        self.start = False
+        self.wall = False
+        self.target = False
+        self.queued = False
+        self.visited = False
+        self.neighbours = []
+        self.prior = None
+    
+    def draw(self, win, color):
+        pygame.draw.rect(win, color, (self.x * square_width, self.y * sqauqre_height, square_width - 2, sqauqre_height - 2))
+
+    def set_neighbours(self):
+        if self.x > 0:
+            self.neighbours.append(grid[self.x - 1][self.y])
+        if self.x < columns - 1:
+            self.neighbours.append(grid[self.x + 1][self.y])
+        if self.y > 0:
+            self.neighbours.append(grid[self.x][self.y - 1])
+        if self.y < rows - 1:
+            self.neighbours.append(grid[self.x][self.y + 1])
+
+for i in range(columns):
+    arr = []
+    for j in range(rows):
+        arr.append(Square(i, j))
+    grid.append(arr)
+print(grid)
+
+for i in range(columns):
+    for j in range(rows):
+        grid[i][j].set_neighbours()
+
+start_square = grid[0][0]
+start_square.start = True
+start_square.visited = True
+queue.append(start_square)
+
+class Maze:
+    def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption('meiro')
+        self.display = pygame.display.set_mode((window_width, window_height))
+        self.clock = pygame.time.Clock()
+        self.font = pygame.font.SysFont("Verdana", 20)
+    
+    def render_fps(self, display):
+        self.text = self.font.render('FPS: ' + str(round(self.clock.get_fps(),2)), True, 'Cyan')
+        display.blit(self.text, (window_width - 100, 0))
+
+    def draw_grid(self, grid):
+        for i in range(columns):
+            for j in range(rows):
+                square = grid[i][j]
+                square.draw(self.display, (20, 20, 20))
+                if square.start:
+                    square.draw(self.display, (0, 200, 200))
+                if square.wall:
+                    square.draw(self.display, (90, 90, 90))
+                if square.target:
+                    square.draw(self.display, (200, 200, 0))
+                if square.queued and not square.start and not square.target:
+                    square.draw(self.display, (200, 0, 0))
+                if square.visited and not square.start and not square.target:
+                    square.draw(self.display, (0, 200, 0))
+                if square in path:
+                    square.draw(self.display, (255, 20, 147))
+
+    def run(self):
+        begin_search = False
+        target_box_set = False
+        searching = True
+        target_box = None
+
+        while True:
+            self.display.fill((0, 0, 0))
+
+            self.draw_grid(grid)
+           
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                #mouse
+                elif event.type == pygame.MOUSEMOTION:
+                    x = pygame.mouse.get_pos()[0]
+                    y = pygame.mouse.get_pos()[1]
+                    if event.buttons[0]:
+                        i = x // square_width
+                        j = y // sqauqre_height
+                        grid[i][j].wall = True
+                    if event.buttons[2] and not target_box_set:
+                        i = x // square_width
+                        j = y // sqauqre_height
+                        target_box = grid[i][j]
+                        target_box.target = True
+                        target_box_set = True
+                #start algo
+                if event.type == pygame.KEYDOWN and target_box_set:
+                    begin_search = True
+                
+            
+            if begin_search:
+                if len(queue) > 0 and searching:
+                    current_square = queue.pop(0)
+                    current_square.visited = True
+                    if current_square == target_box:
+                        searching = False
+                        while current_square.prior != start_square:
+                            path.append(current_square.prior)
+                            current_square = current_square.prior
+                    else:
+                        for neighbour in current_square.neighbours:
+                            if not neighbour.queued and not neighbour.wall:
+                                neighbour.queued = True
+                                neighbour.prior = current_square
+                                queue.append(neighbour)
+                else:
+                    if searching:
+                        print("no solution")
+                        searching = False
+
+            
+            self.render_fps(self.display)
+            pygame.display.update()
+            self.clock.tick(60)
+if __name__ == "__main__":
+    Maze().run()
